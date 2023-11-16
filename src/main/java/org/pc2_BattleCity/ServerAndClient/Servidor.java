74c4534112a932ab30f65730e08876707d6be5e3
kenyi <kenyialcantara.ha@gmail.com>
2023-11-16 10:40:59 -0500

    modificando para ver

--- src/main/java/org/pc2_BattleCity/ServerAndClient/Servidor.java
+++ src/main/java/org/pc2_BattleCity/ServerAndClient/Servidor.java
@@ -94,6 +94,7 @@ public class Servidor {
         for (ConexionCliente cliente : clientesConectados) {
             try {
                 cliente.salida.writeObject(message);
+                System.out.println("->"+cliente.idClient);
             } catch (IOException e) {
                 System.err.println("Error al enviar mensaje a cliente: " + e.getMessage());
             }
